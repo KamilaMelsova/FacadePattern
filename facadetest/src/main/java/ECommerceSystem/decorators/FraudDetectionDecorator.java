e3f5d22b7359c7caa91cd92577b087340ca30932
KamilaMelsova <240637@astanait.edu.kz>
2025-10-17 19:49:13 +0500

    FraudDetectionDecorator.java

--- facadetest/src/main/java/ECommerceSystem/decorators/FraudDetectionDecorator.java
+++ facadetest/src/main/java/ECommerceSystem/decorators/FraudDetectionDecorator.java
@@ -2,12 +2,10 @@ package ECommerceSystem.decorators;
 import ECommerceSystem.factory.Payment;
 
 public class FraudDetectionDecorator extends PaymentDecorator{
-    public FraudDetectionDecorator(Payment payment){
-        super(payment);
-    }
+    public FraudDetectionDecorator(Payment payment){super(payment);}
     public void pay(double amount){
         System.out.println("Checking for fraud before payment...");
         super.pay(amount);
-        System.out.println("No fraud detected");
-    }
+        System.out.println("No fraud detected");}
 }
+
