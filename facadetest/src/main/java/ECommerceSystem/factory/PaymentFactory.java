4452cb31adbf1a58a56a12f05559c032412ddbf7
KamilaMelsova <240637@astanait.edu.kz>
2025-10-17 19:52:26 +0500

    PaymentFactory.java

--- facadetest/src/main/java/ECommerceSystem/factory/PaymentFactory.java
+++ facadetest/src/main/java/ECommerceSystem/factory/PaymentFactory.java
@@ -1,7 +1,6 @@
 package ECommerceSystem.factory;
 
-public class PaymentFactory{
-    public static Payment createPayment(String type){
+public class PaymentFactory{public static Payment createPayment(String type){
         if (type.equalsIgnoreCase("credit")){return new CreditCardPayment();}
         else if (type.equalsIgnoreCase("paypal")){return new PayPalPayment();}
         else{throw new IllegalArgumentException("Unknown payment type: " + type);}
