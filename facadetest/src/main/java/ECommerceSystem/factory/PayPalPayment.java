f9ea68a15a559b0fe703c70d1a6cb15b4dde1332
KamilaMelsova <240637@astanait.edu.kz>
2025-10-17 19:51:47 +0500

    PayPalPayment.java

--- facadetest/src/main/java/ECommerceSystem/factory/PayPalPayment.java
+++ facadetest/src/main/java/ECommerceSystem/factory/PayPalPayment.java
@@ -1,7 +1,5 @@
 package ECommerceSystem.factory;
-public class PayPalPayment implements Payment{
-    public void pay(double amount){
+public class PayPalPayment implements Payment{public void pay(double amount){
         System.out.println("Paid " + amount + " tenge via PayPal");
     }
 }
-
