48c5f85b74b3a74f03bb9ae5c1cbd548d7b880ab
KamilaMelsova <240637@astanait.edu.kz>
2025-10-17 19:51:27 +0500

    CreditCardPayment.java

--- facadetest/src/main/java/ECommerceSystem/factory/CreditCardPayment.java
+++ facadetest/src/main/java/ECommerceSystem/factory/CreditCardPayment.java
@@ -1,6 +1,5 @@
 package ECommerceSystem.factory;
-public class CreditCardPayment implements Payment{
-    public void pay(double amount){
+public class CreditCardPayment implements Payment{public void pay(double amount){
         System.out.println("Paid " + amount + " tenge with Credit Card");
     }
 }
